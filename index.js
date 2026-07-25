85ec43b735ccbee2357a7393c5d4af3f2fc95adc
donotfalter-lab <hjamyll@yahoo.com>
2026-07-24 23:13:25 -0400

    asynclab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+async function fetchPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    displayPosts(posts);
+}
+
+function displayPosts(posts) {
+    const postList = document.getElementById("post-list");
+
+    posts.forEach((post) => {
+        const li = document.createElement("li");
+        const h1 = document.createElement("h1");
+        const p = document.createElement("p");
+
+        h1.textContent = post.title;
+        p.textContent = post.body;
+
+        li.append(h1, p);
+        postList.append(li);
+    });
+}
+
+fetchPosts();
